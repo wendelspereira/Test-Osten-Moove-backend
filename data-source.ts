4a53a37c3689cb4725f73befa1e0f89e555df95c
Wendel S. Pereira <wendelspereira@outlook.com>
2022-07-29 09:51:02 -0300

    'deploy'

--- data-source.ts
+++ data-source.ts
@@ -1,14 +1,14 @@
 import { DataSource } from "typeorm";
 import { Business } from "./src/modules/business/infra/sqlite/entity/Business";
 import "dotenv/config";
-
+const env = process.env.ENVIRONMENT
 export const AppDataSource = new DataSource({
   type: "better-sqlite3",
   database: "./database/database.sqlite",
   synchronize: true,
   logging: true,
   entities: [Business, "__dirname + '/src/modules/business/infra/sqlite/entity/*.{js,ts}'"],
-  migrations: ["./build/src/shared/infra/typeorm/migrations/*.{js,ts}", "./src/shared/infra/typeorm/migrations/*.{js,ts}"],
+  migrations: env === "production" ? ["./build/src/shared/infra/typeorm/migrations/*.js"] : ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
 
 
